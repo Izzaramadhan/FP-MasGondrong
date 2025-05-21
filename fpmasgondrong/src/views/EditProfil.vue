4704f8a338878817f13c72e3da95af0ea5dcc7a3
Ubayahmad11 <ubayahmad@students.amikom.ac.id>
2025-05-21 20:33:41 +0700

    Baru

---
+++ fpmasgondrong/src/views/EditProfil.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-5">
+    <div class="login-box">
+      <h4 class="text-center mb-4">Edit Profil Pembeli</h4>
+      <form @submit.prevent="submitForm">
+        <div class="mb-3" v-for="(field, index) in fields" :key="index">
+          <label>{{ field.label }}</label>
+          <component :is="field.type === 'textarea' ? 'textarea' : 'input'"
+                     class="form-control"
+                     :type="field.inputType"
+                     v-model="form[field.model]"
+                     :rows="field.rows || null" />
+        </div>
+        <button type="submit" class="btn btn-green w-100">Simpan Perubahan</button>
+      </form>
+    </div>
+  </div>
+</template>
