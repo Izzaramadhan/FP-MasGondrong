abc83afa1f58f0a346f5c4f0f0b4ee60ba3eeaa4
DanangWijayato08 <wijayantodanang867@gmail.com>
2025-05-21 21:42:03 +0700

    New

--- fpmasgondrong/src/App.vue
+++ fpmasgondrong/src/App.vue
@@ -7,3 +7,15 @@
     <AppFooter />
   </div>
 </template>
+
+<script>
+import AppHeader from '@/components/Header.vue'
+import AppFooter from '@/components/Footer.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
