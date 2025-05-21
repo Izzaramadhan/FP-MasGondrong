182e265db67974e8cd40fd89fe57bbda076b6221
Ubayahmad11 <ubayahmad@students.amikom.ac.id>
2025-05-21 20:21:15 +0700

    Baru

---
+++ fpmasgondrong/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header-inner d-flex justify-content-between align-items-center">
+      <img src="@/assets/images/logobaru2.png" alt="Logo Rental" class="img-logo" />
+      <span class="brand">MasGondrong Rent</span>
+    </div>
+
+    <div class="right">
+      <nav class="navbar">
+        <router-link to="/">Beranda</router-link>
+        <router-link to="/kelompok">Tentang Kami</router-link>
+        <router-link to="/cara-sewa">Cara Sewa</router-link>
+      </nav>
+
+      <div class="btn-group">
+        <router-link v-if="!isLoggedIn" to="/register" class="btn-pesan">Register</router-link>
+        <router-link v-if="!isLoggedIn" to="/login" class="btn-login">Login</router-link>
+        <button v-else @click="logout" class="btn-login">Logout</button>
+      </div>
+    </div>
+  </header>
+</template>
