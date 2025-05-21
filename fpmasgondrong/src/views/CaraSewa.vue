06dd0de21e424a326e0bc0ac9d9618f87badf95c
Ubayahmad11 <ubayahmad@students.amikom.ac.id>
2025-05-21 20:30:34 +0700

    Baru

--- fpmasgondrong/src/views/CaraSewa.vue
+++ fpmasgondrong/src/views/CaraSewa.vue
@@ -10,3 +10,34 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'CaraSewa',
+  setup() {
+    const langkahSewa = [
+      {
+        judul: 'Pilih Kendaraan',
+        deskripsi: 'Telusuri halaman menu untuk memilih kendaraan yang ingin Anda sewa sesuai dengan kebutuhan Anda.'
+      },
+      {
+        judul: 'Isi Form Pemesanan',
+        deskripsi: 'Klik "Sewa Sekarang" lalu lengkapi data pemesanan seperti tanggal mulai dan selesai sewa.'
+      },
+      {
+        judul: 'Lakukan Pembayaran',
+        deskripsi: 'Setelah mengisi form, Anda akan diarahkan ke halaman pembayaran. Pilih metode pembayaran dan konfirmasi.'
+      },
+      {
+        judul: 'Konfirmasi & Verifikasi',
+        deskripsi: 'Tim kami akan menghubungi Anda untuk memverifikasi dan mengonfirmasi pesanan Anda.'
+      },
+      {
+        judul: 'Nikmati Perjalanan Anda',
+        deskripsi: 'Kendaraan akan diantar atau bisa Anda ambil sesuai kesepakatan. Selamat berkendara!'
+      }
+    ]
+    return { langkahSewa }
+  }
+}
+</script>
