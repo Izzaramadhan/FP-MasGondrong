c9efda2f8358bdfb3f53a24e9876af110d10a5e4
Ubayahmad11 <ubayahmad@students.amikom.ac.id>
2025-05-21 20:15:28 +0700

    Baru

---
+++ fpmasgondrong/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="footer">
+        <div class="footer-container">
+        <!-- Kolom Logo dan Kontak -->
+        <div class="footer-column">
+            <img src="@/assets/images/logobaru2.png" alt="Logo" class="footer-logo" />
+            <p>MasGondrongrent@gmail.com</p>
+            <p>+62 917-134-1422</p>
+            <p>@MasGondrongrent</p>
+        </div>
+        </div>
+    </footer>
+</template>
